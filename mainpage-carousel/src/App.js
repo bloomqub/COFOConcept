810d660d0cb63267970237f04fa092701bda4392
BrockBloomquist <72510382+bloomqub@users.noreply.github.com>
2023-03-01 00:06:13 -0600

    Changed a Carousel photo and added a Navigation Bar to the App.js functional component.

--- mainpage-carousel/src/App.js
+++ mainpage-carousel/src/App.js
@@ -1,9 +1,11 @@
 import ".//App.css";
 import Slider from "./Components/Slider";
+import NavigationBar from "./Components/NavigationBar";
 
 function UncontrolledExample() {
   return (
     <>
+      <NavigationBar></NavigationBar>
       <Slider></Slider>
     </>
   );
--- mainpage-carousel/src/Components/Slider.js
+++ mainpage-carousel/src/Components/Slider.js
@@ -7,9 +7,11 @@ const Slider = () => {
       <Carousel>
         <Carousel.Item>
           <img
-            className="c"
+            class="c"
             src="https://hips.hearstapps.com/hmg-prod/images/tropical-beach-at-sunset-royalty-free-image-1655673364.jpg"
             alt="First slide"
+            width="2121"
+            height="1414"
           />
           <Carousel.Caption>
             <h3>Welcome to COFO Concept!</h3>
@@ -18,9 +20,11 @@ const Slider = () => {
         </Carousel.Item>
         <Carousel.Item>
           <img
-            className="c"
-            src="https://ssl.tzoo-img.com/images/blog/legacyblog/us/wp-content/uploads/2018/02/6_WhitehavenBeach_Australia_shutterstock_378831505.jpg?width=412&spr=3"
+            class="c"
+            src="https://www.travelandleisure.com/thmb/uLqruTjACardjIBe7FK8tMfA6ZM=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/elafonissi-beach-crete-greece-WRLDBEACH0421-50fd96fe8e5e45448d154ae43b38b855.jpg"
             alt="Second slide"
+            width="2121"
+            height="1414"
           />
 
           <Carousel.Caption>
@@ -30,9 +34,11 @@ const Slider = () => {
         </Carousel.Item>
         <Carousel.Item>
           <img
-            className="c"
+            class="c"
             src="https://www.brettdeacon.com/images/xl/PrettyBeachSunrise1.jpg"
             alt="Third slide"
+            width="2121"
+            height="1414"
           />
 
           <Carousel.Caption>
