1cc7aea6825a2158779034b94c6bfb6d133c0c85
bloomqub <72510382+bloomqub@users.noreply.github.com>
2023-03-03 12:16:39 -0600

    Created a css file and modified css files
    Changed the image size of the Images on the carousel

--- mainpage-carousel/src/Components/Slider.js
+++ mainpage-carousel/src/Components/Slider.js
@@ -1,30 +1,34 @@
 import React from "react";
 import Carousel from "react-bootstrap/Carousel";
-
+import ".//CSSCOMPONENTS/Slider.css"
 const Slider = () => {
   return (
     <>
+      <div style={{
+          textAlign: "center",
+           fontSize: "18px"
+      }}></div>
       <Carousel>
         <Carousel.Item>
           <img
-            class="c"
+            className="c"
             src="https://hips.hearstapps.com/hmg-prod/images/tropical-beach-at-sunset-royalty-free-image-1655673364.jpg"
             alt="First slide"
-            width="2121"
-            height="1414"
+            width="100%"
+            height="1000px"
           />
           <Carousel.Caption>
-            <h3>Welcome to COFO Concept!</h3>
-            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+              <h3>Welcome to COFO Concept!</h3>
+              <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
           </Carousel.Caption>
         </Carousel.Item>
         <Carousel.Item>
           <img
-            class="c"
+            className="c"
             src="https://www.travelandleisure.com/thmb/uLqruTjACardjIBe7FK8tMfA6ZM=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/elafonissi-beach-crete-greece-WRLDBEACH0421-50fd96fe8e5e45448d154ae43b38b855.jpg"
             alt="Second slide"
-            width="2121"
-            height="1414"
+            width="100%"
+            height="1000px"
           />
 
           <Carousel.Caption>
@@ -34,11 +38,11 @@ const Slider = () => {
         </Carousel.Item>
         <Carousel.Item>
           <img
-            class="c"
+            className="c"
             src="https://www.brettdeacon.com/images/xl/PrettyBeachSunrise1.jpg"
             alt="Third slide"
-            width="2121"
-            height="1414"
+            width="100%"
+            height="1000px"
           />
 
           <Carousel.Caption>
