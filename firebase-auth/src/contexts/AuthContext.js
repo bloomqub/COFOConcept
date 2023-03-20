ebd0beda9876b6be884020cca00ca9693ad57d12
ojontac <70986574+ojontac@users.noreply.github.com>
2023-03-20 10:46:41 -0500

    Create AuthContext.js
    1st commit

---
+++ firebase-auth/src/contexts/AuthContext.js
@@ -0,0 +1,42 @@
+//import { onAuthStateChanged } from 'firebase/auth'
+import React, { useContext, useState, useEffect } from 'react'
+import { auth } from '../firebase'
+
+const AuthContext = React.createContext()
+
+// allows us to use auth context function
+export function useAuth(){
+    return useContext(AuthContext)  
+}
+
+// returns the values we want to provide for authentication
+export function AuthProvider({ children }) {
+    const [currentUser, setCurrentUser] = useState()
+    const [loading, setLoading] = useState(true)  //verification to see if there is already user
+
+    // cretes a user instance when signup occurs
+    function signup(email, password) {
+        return auth.createUserWithEmailAndPassword(email, password) 
+    }
+
+    useEffect(() => {
+        //allows us to set the user
+        const unsubscribe = auth.onAuthStateChanged(user => {
+            setCurrentUser(user)
+            setLoading(false)
+        })
+        return unsubscribe
+    }, [])
+
+    const value = {
+        currentUser,
+        signup
+    }  
+
+    return (
+        // makes sure not to render application until the user is set in useEffect
+        <AuthContext.Provider value= {value}>
+            {!loading && children}
+        </AuthContext.Provider>
+    )
+}
