0f263861478460d0e7aa0c546fb360fb318cd876
bloomqub <72510382+bloomqub@users.noreply.github.com>
2023-03-20 12:03:54 -0500

    Added image to footer and install router package

--- mainpage-carousel/src/Components/Footer.js
+++ mainpage-carousel/src/Components/Footer.js
@@ -1,9 +1,13 @@
 import React, { Component } from 'react'
+import Picture from "../Images/The CoFo Concept.png"
 
 export default class Footer extends Component {
   render() {
     return (
       <div className="footer-container">
+        <div className="image-container">
+            <img src={Picture} alt="LogoPicture"/>
+        </div>
         <div className="footer-text">
             <h1>Footer</h1>
         </div>
