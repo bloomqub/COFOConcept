9a46819057e4af50997865d81069e95f88254f27
BrockBloomquist <72510382+bloomqub@users.noreply.github.com>
2023-03-23 00:54:58 -0500

    Added new dimensions for the images and changed one to one of Courtney

--- mainpage-carousel/src/Components/Slider.js
+++ mainpage-carousel/src/Components/Slider.js
@@ -1,38 +1,42 @@
 import React from "react";
 import Carousel from "react-bootstrap/Carousel";
-import ".//CSSCOMPONENTS/Slider.css"
+import ".//CSSCOMPONENTS/Slider.css";
+import Picture from "../Images/beach 2.JPG";
 const Slider = () => {
   return (
     <>
-      <div style={{
+      <div
+        style={{
           textAlign: "center",
-           fontSize: "18px"
-      }}></div>
+          fontSize: "18px",
+        }}
+      ></div>
       <Carousel>
         <Carousel.Item>
-          <img
-            className="c"
-            src="https://hips.hearstapps.com/hmg-prod/images/tropical-beach-at-sunset-royalty-free-image-1655673364.jpg"
-            alt="First slide"
-            width="100%"
-            height="500px"
-          />
+          <div className="c">
+            <img src={Picture} alt="beach 2.JPG" />
+          </div>
           <Carousel.Caption>
-              <h3>Welcome to COFO Concept!</h3>
-              <p></p>
+            <h3>Welcome to COFO Concept!</h3>
+            <p></p>
           </Carousel.Caption>
         </Carousel.Item>
         <Carousel.Item>
-          <img
-            className="c"
-            src="https://www.travelandleisure.com/thmb/uLqruTjACardjIBe7FK8tMfA6ZM=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/elafonissi-beach-crete-greece-WRLDBEACH0421-50fd96fe8e5e45448d154ae43b38b855.jpg"
-            alt="Second slide"
-            width="100%"
-            height="500px"
-          />
+          <div>
+            <img
+              className="c"
+              src="https://www.travelandleisure.com/thmb/uLqruTjACardjIBe7FK8tMfA6ZM=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/elafonissi-beach-crete-greece-WRLDBEACH0421-50fd96fe8e5e45448d154ae43b38b855.jpg"
+              alt="Second slide"
+              width="100%"
+              height="772"
+            />
+          </div>
 
           <Carousel.Caption>
-            <h3>“Success usually comes to those who are too busy to be looking for it.”</h3>
+            <h3>
+              “Success usually comes to those who are too busy to be looking for
+              it.”
+            </h3>
             <p>.</p>
           </Carousel.Caption>
         </Carousel.Item>
@@ -42,14 +46,12 @@ const Slider = () => {
             src="https://www.brettdeacon.com/images/xl/PrettyBeachSunrise1.jpg"
             alt="Third slide"
             width="100%"
-            height="500px"
+            height="772"
           />
 
           <Carousel.Caption>
             <h3>"The body achieves what the mind believes."</h3>
-            <p>
-              
-            </p>
+            <p></p>
           </Carousel.Caption>
         </Carousel.Item>
       </Carousel>
