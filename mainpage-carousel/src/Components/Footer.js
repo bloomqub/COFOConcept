a93606fb74d952c35ec7d110b340b5ebd6b06d3e
Chris Smith Jr <89213845+ChrisS561@users.noreply.github.com>
2023-03-26 01:20:22 -0500

    Footer is completed
    Footer is completed and the slider has images attached to them.

--- mainpage-carousel/src/Components/Footer.js
+++ mainpage-carousel/src/Components/Footer.js
@@ -1,13 +1,76 @@
-import React, { Component } from 'react'
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+	faFacebook,
+	faTwitter,
+	faInstagram,
+	faLinkedin,
+	faYoutube,
+	faTiktok,
+} from "@fortawesome/free-brands-svg-icons";
+// import picture from '../Images/The CoFo Concept 2.png';
+export default function Footer() {
+	return (
+		<footer className="bg-white text-center text-white">
+			<div className="container p-4 pb-0">
+				<section className="mb-4">
+					{/* <img src={picture} alt='Logo' style={{height: '50px'}} /> */}
+					<a
+						href="#Facebook"
+						className="btn btn-primary btn-floating m-1"
+						style={{ backgroundColor: "#3b5998", borderRadius: "50%" }}
+					>
+						<FontAwesomeIcon icon={faFacebook} />
+					</a>
 
-export default class Footer extends Component {
-  render() {
-    return (
-      <div className="footer-container">
-        <div className="footer-text">
-            <h1>Footer</h1>
-        </div>
-        </div>
-    )
-  }
+					<a
+						href="#Twitter"
+						className="btn btn-primary btn-floating m-1"
+						style={{ backgroundColor: "#55acee", borderRadius: "50%" }}
+					>
+						<FontAwesomeIcon icon={faTwitter} />
+					</a>
+
+					<a
+						href="#Instagram"
+						className="btn btn-primary btn-floating m-1"
+						style={{ backgroundColor: "#ac2bac", borderRadius: "50%" }}
+					>
+						<FontAwesomeIcon icon={faInstagram} />
+					</a>
+
+					<a
+						href="#Linkedin"
+						className="btn btn-primary btn-floating m-1"
+						style={{ backgroundColor: "#0082ca", borderRadius: "50%" }}
+					>
+						<FontAwesomeIcon icon={faLinkedin} />
+					</a>
+
+					<a
+						href="#Youtube"
+						className="btn btn-primary btn-floating m-1"
+						style={{ backgroundColor: "#FF0000", borderRadius: "50%" }}
+					>
+						<FontAwesomeIcon icon={faYoutube} />
+					</a>
+
+					<a
+						href="#Tiktok"
+						className="btn btn-primary btn-floating m-1"
+						style={{ backgroundColor: "#000000", borderRadius: "50%" }}
+					>
+						<FontAwesomeIcon icon={faTiktok} />
+					</a>
+				</section>
+			</div>
+
+			<div
+				className="text-center p-3"
+				style={{ backgroundColor: "rgba(0, 0, 0, 1)" }}
+			>
+				© {new Date().getFullYear()} CoFo Concept
+			</div>
+		</footer>
+	);
 }
--- mainpage-carousel/src/Components/Slider.js
+++ mainpage-carousel/src/Components/Slider.js
@@ -1,6 +1,9 @@
 import React from "react";
 import Carousel from "react-bootstrap/Carousel";
 import ".//CSSCOMPONENTS/Slider.css"
+import Picture from "/Users/chris/Documents/GitHub/COFOConcept/mainpage-carousel/src/Images/beach6.JPG"
+import GymPic from "/Users/chris/Documents/GitHub/COFOConcept/mainpage-carousel/src/Images/pool pic 7.JPG"
+import ThirdPic from "/Users/chris/Documents/GitHub/COFOConcept/mainpage-carousel/src/Images/pool pic 3.JPG"
 const Slider = () => {
   return (
     <>
@@ -11,44 +14,43 @@ const Slider = () => {
       <Carousel>
         <Carousel.Item>
           <img
-            className="c"
-            src="https://hips.hearstapps.com/hmg-prod/images/tropical-beach-at-sunset-royalty-free-image-1655673364.jpg"
+            className="FirstSlide"
+            src={Picture}
             alt="First slide"
             width="100%"
             height="500px"
           />
           <Carousel.Caption>
-              <h3>Welcome to COFO Concept!</h3>
-              <p></p>
+              <h2>Welcome to CoFo Concept!</h2>
+              <p>Testing</p>
           </Carousel.Caption>
         </Carousel.Item>
         <Carousel.Item>
           <img
-            className="c"
-            src="https://www.travelandleisure.com/thmb/uLqruTjACardjIBe7FK8tMfA6ZM=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/elafonissi-beach-crete-greece-WRLDBEACH0421-50fd96fe8e5e45448d154ae43b38b855.jpg"
+            className="SecondSlide"
+            src={GymPic}
             alt="Second slide"
             width="100%"
             height="500px"
           />
 
           <Carousel.Caption>
-            <h3>“Success usually comes to those who are too busy to be looking for it.”</h3>
-            <p>.</p>
+            <h2>“Success usually comes to those who are too busy to be looking for it.”</h2>
+            <p>Testing</p>
           </Carousel.Caption>
         </Carousel.Item>
         <Carousel.Item>
           <img
-            className="c"
-            src="https://www.brettdeacon.com/images/xl/PrettyBeachSunrise1.jpg"
+            className="ThirdSlide"
+            src={ThirdPic}
             alt="Third slide"
             width="100%"
             height="500px"
           />
 
           <Carousel.Caption>
-            <h3>"The body achieves what the mind believes."</h3>
-            <p>
-              
+            <h2>"The body achieves what the mind believes."</h2>
+            <p>Testing
             </p>
           </Carousel.Caption>
         </Carousel.Item>
