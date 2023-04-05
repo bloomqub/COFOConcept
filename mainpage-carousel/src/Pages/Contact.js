20a60dd3f9d2422a5ad33b5ea8d07a7e8306aaa7
Chris Smith Jr <89213845+ChrisS561@users.noreply.github.com>
2023-04-05 13:28:53 -0500

    Updated the Contact and OndemandPAge

--- mainpage-carousel/src/Pages/Contact.js
+++ mainpage-carousel/src/Pages/Contact.js
@@ -1,13 +1,59 @@
-import React from "react";
-import NavigationBarFinal from "../Components/NavigationBarFinal";
-const Contact = () => {
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import FloatingLabel from 'react-bootstrap/FloatingLabel'
+import NavigationBarFinal from '../Components/NavigationBarFinal';
+
+function Contact() {
   return (
     <>
-    <NavigationBarFinal />
-    <div>
-      <h1>Test :D #2</h1>
-    </div>
+    <NavigationBarFinal/>
+    <Form>
+      <div> <h1>Contact me!</h1> </div>
+      <p> If you have any questions fill out the form below</p>
+      <Form.Group className="mb-3" controlId="ContactPageform">
+        <Form.Label>Name</Form.Label>
+        <Form.Control type="Name" placeholder="Name" />
+        <Form.Text className="text-muted">
+  
+        </Form.Text>
+      </Form.Group>
+
+      <Form.Group className="mb-3" controlId="formBasicEmail">
+        <Form.Label>Email</Form.Label>
+        <Form.Control type="Email" placeholder="Email Address" />
+        <Form.Text className="text-muted">
+
+        </Form.Text>
+      </Form.Group>
+
+      <Form.Group className="mb-3" controlId="formBasicSubject">
+        <Form.Label>Subject</Form.Label>
+        <Form.Control type="Subject" placeholder="Subject" />
+        <Form.Text className="text-muted">
+        
+        </Form.Text>
+      </Form.Group>
+
+      <Form.Group className="mb-3" controlId="formBasicComments ">
+        <Form.Label>Comments</Form.Label>
+      <FloatingLabel controlId="floatingTextarea2" label="Comments">
+        <Form.Control
+          as="textarea"
+          placeholder="Leave a comment here"
+          style={{ height: '100px' }}
+        />
+      </FloatingLabel>
+      </Form.Group>
+
+      <Form.Group className="mb-3" controlId="formBasicCheckbox">
+        <Form.Check type="checkbox" label="Send me copy" />
+      </Form.Group>
+      <Button variant="primary" type="submit">
+        Submit
+      </Button>
+    </Form>
     </>
   );
-};
+}
+
 export default Contact;
--- mainpage-carousel/src/Pages/OnDemandPage.js
+++ mainpage-carousel/src/Pages/OnDemandPage.js
@@ -1,14 +1,70 @@
 import React from "react";
+import NavigationBar from "../Components/NavigationBar";
+import PropTypes from "prop-types";
+import "./CSSPages/OnDemandPage.css"
 import NavigationBarFinal from "../Components/NavigationBarFinal";
 
-const OnDemand = () => {
+const OnDemandPage = () => {
   return (
     <>
-    <NavigationBarFinal />
-    <div>
-      <h1>Test :D</h1>
-    </div> 
+      <NavigationBarFinal />
+      <section className="header">
+      </section>
+      <section className="Recent">
+        <h2>Recent</h2>
+        <FitnessProgramGrid numPrograms={7} />
+      </section>
+      <section className="Fullbody">
+        <h2>Full Body</h2>
+        <FitnessProgramGrid numPrograms={7} />
+      </section>
+      <section className="Upperbody">
+        <h2>Upper Body</h2>
+        <FitnessProgramGrid numPrograms={7} />
+      </section>
+      <section className="Lowerbody">
+        <h2>Lower Body</h2>
+        <FitnessProgramGrid numPrograms={7} />
+      </section>
+      <section className="Core">
+        <h2>Core</h2>
+        <FitnessProgramGrid numPrograms={7} />
+      </section>
     </>
   );
 };
-export default OnDemand;
+
+function FitnessProgramGrid({ numPrograms }) {
+  return (
+    <div className="row flex-nowrap overflow-auto">
+      {[...Array(numPrograms)].map((_, idx) => (
+        <FitnessProgramCardItem key={idx} />
+      ))}
+    </div>
+  );
+}
+
+FitnessProgramGrid.propTypes = {
+  numPrograms: PropTypes.number.isRequired,
+};
+
+function FitnessProgramCardItem() {
+  return (
+    <div className="col">
+      <div className="card h-100">
+        <img
+          src="holder.js/100px160"
+          alt="Video Thumbnail"
+          className="card-img-top img-fluid"
+        />
+        <div className="card-body">
+          <h5 className="card-title">Video Title</h5>
+          <p className="card-text"></p>
+          <button className="btn btn-primary">Buy Now</button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default OnDemandPage;
