62ec96f71783a1746d8df6d4b6ad002e1f7da1b7
Chris Smith Jr <89213845+ChrisS561@users.noreply.github.com>
2023-04-06 18:53:30 -0500

    Updated the OndemandPage and the css file
    Found a better way to display the videos

--- mainpage-carousel/src/Pages/OnDemandPage.js
+++ mainpage-carousel/src/Pages/OnDemandPage.js
@@ -1,70 +1,88 @@
 import React from "react";
-import NavigationBar from "../Components/NavigationBar";
-import PropTypes from "prop-types";
-import "./CSSPages/OnDemandPage.css"
 import NavigationBarFinal from "../Components/NavigationBarFinal";
+import PropTypes from "prop-types";
+import "./CSSPages/OnDemandPage.css";
 
 const OnDemandPage = () => {
-  return (
-    <>
-      <NavigationBarFinal />
-      <section className="header">
-      </section>
-      <section className="Recent">
-        <h2>Recent</h2>
-        <FitnessProgramGrid numPrograms={7} />
-      </section>
-      <section className="Fullbody">
-        <h2>Full Body</h2>
-        <FitnessProgramGrid numPrograms={7} />
-      </section>
-      <section className="Upperbody">
-        <h2>Upper Body</h2>
-        <FitnessProgramGrid numPrograms={7} />
-      </section>
-      <section className="Lowerbody">
-        <h2>Lower Body</h2>
-        <FitnessProgramGrid numPrograms={7} />
-      </section>
-      <section className="Core">
-        <h2>Core</h2>
-        <FitnessProgramGrid numPrograms={7} />
-      </section>
-    </>
-  );
+	return (
+		<>
+			<NavigationBarFinal />
+			<section className="header"></section>
+			<section className="category">
+				<h2>Recent</h2>
+				<FitnessProgramGrid
+					numPrograms={7}
+					thumbnailUrl="https://via.placeholder.com/150"
+				/>
+			</section>
+			<section className="category">
+				<h2>Full Body</h2>
+				<FitnessProgramGrid
+					numPrograms={7}
+					thumbnailUrl="https://via.placeholder.com/150"
+				/>
+			</section>
+			<section className="category">
+				<h2>Upper Body</h2>
+				<FitnessProgramGrid
+					numPrograms={7}
+					thumbnailUrl="https://via.placeholder.com/150"
+				/>
+			</section>
+			<section className="category">
+				<h2>Lower Body</h2>
+				<FitnessProgramGrid
+					numPrograms={7}
+					thumbnailUrl="https://via.placeholder.com/150"
+				/>
+			</section>
+			<section className="category">
+				<h2>Core</h2>
+				<FitnessProgramGrid
+					numPrograms={7}
+					thumbnailUrl="https://via.placeholder.com/150"
+				/>
+			</section>
+		</>
+	);
 };
 
-function FitnessProgramGrid({ numPrograms }) {
-  return (
-    <div className="row flex-nowrap overflow-auto">
-      {[...Array(numPrograms)].map((_, idx) => (
-        <FitnessProgramCardItem key={idx} />
-      ))}
-    </div>
-  );
+function FitnessProgramGrid({ numPrograms, thumbnailUrl }) {
+	return (
+		<div className="row flex-nowrap overflow-auto">
+			{[...Array(numPrograms)].map((_, idx) => (
+				<FitnessProgramCardItem key={idx} thumbnailUrl={thumbnailUrl} />
+			))}
+		</div>
+	);
 }
 
 FitnessProgramGrid.propTypes = {
-  numPrograms: PropTypes.number.isRequired,
+	numPrograms: PropTypes.number.isRequired,
+	thumbnailUrl: PropTypes.string.isRequired,
 };
 
-function FitnessProgramCardItem() {
-  return (
-    <div className="col">
-      <div className="card h-100">
-        <img
-          src="holder.js/100px160"
-          alt="Video Thumbnail"
-          className="card-img-top img-fluid"
-        />
-        <div className="card-body">
-          <h5 className="card-title">Video Title</h5>
-          <p className="card-text"></p>
-          <button className="btn btn-primary">Buy Now</button>
-        </div>
-      </div>
-    </div>
-  );
+function FitnessProgramCardItem({ thumbnailUrl }) {
+	return (
+		<div className="col">
+			<div className="card h-100">
+				<img
+					src={thumbnailUrl}
+					alt="Video Thumbnail"
+					className="card-img-top img-fluid"
+				/>
+				<div className="card-body">
+					<h5 className="card-title">Video Title</h5>
+					<p className="card-text"></p>
+					<button className="btn btn-primary">Buy Now</button>
+				</div>
+			</div>
+		</div>
+	);
 }
+
+FitnessProgramCardItem.propTypes = {
+	thumbnailUrl: PropTypes.string.isRequired,
+};
 
 export default OnDemandPage;
