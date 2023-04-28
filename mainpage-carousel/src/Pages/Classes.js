d590994a42937b4eb8a76a9d962d26db8f79298a
Cactus-Wren <70667143+Cactus-Wren@users.noreply.github.com>
2023-04-28 16:33:11 +0000

    Updated Classes.js

--- mainpage-carousel/src/Pages/Classes.js
+++ mainpage-carousel/src/Pages/Classes.js
@@ -41,7 +41,7 @@ const Classes = () => {
 						<div className="card mb-4">
 							<div className="card-body">
 								<h4 className="card-title text-center">30 Day All-Access Pass</h4>
-								<p className="card-text text-center">30 days of unlimited access to classes</p>
+								<p className="card-text text-center">30 days of unlimited access to classes and videos</p>
 								<h5 className="card-text text-center">$75</h5>
 								{/* Add a photo here? */}
 							</div>
@@ -80,72 +80,25 @@ const Classes = () => {
 						</div>
 						<div className="row justify-content-center">
 							<div className="col-md-4 text-center">
-							<form
-									action="https://www.paypal.com/cgi-bin/webscr"
-									method="post"
-									target="_top"
-								>
-									<input type="hidden" name="cmd" value="_s-xclick"></input>
-									<input
-										type="hidden"
-										name="hosted_button_id"
-										value="VBTC9Y4BMEMYN"
-									></input>
-									<table>
-										<tr>
-											<td>
-												<input
-													type="hidden"
-													name="on0"
-													value="Select Class Here"
-												></input>
-												Select Class Here
-											</td>
-										</tr>
-										<tr>
-											<td>
-												<select name="os0">
-													<option value="Single Class">
-														Single Class $10.00 USD
-													</option>
-													<option value="5 Class Package">
-														5 Class Package $40.00 USD
-													</option>
-													<option value="10 Class Package">
-														10 Class Package $60.00 USD
-													</option>
-													<option value="30 Day Access Pass">
-														30 Day Access Pass $75.00 USD
-													</option>
-													<option value="2 week Challenge">
-														2 week Challenge $50.00 USD
-													</option>
-													<option value="4 week Challenge">
-														4 week Challenge $80.00 USD
-													</option>
-													<option value="6 week Challenge">
-														6 week Challenge $100.00 USD
-													</option>
-												</select>
-											</td>
-										</tr>
-									</table>
-									<input type="hidden" name="currency_code" value="USD"></input>
-									<input
-										type="image"
-										src="https://www.paypalobjects.com/en_US/i/btn/btn_paynowCC_LG.gif"
-										border="0"
-										name="submit"
-										alt="PayPal - The safer, easier way to pay online!"
-									></input>
-									<img
-										alt=""
-										border="0"
-										src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif"
-										width="1"
-										height="1"
-									></img>
-								</form>
+							<form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
+								<input type="hidden" name="cmd" value="_s-xclick"></input>
+								<input type="hidden" name="hosted_button_id" value="VBTC9Y4BMEMYN"></input>
+								<table>
+									<tr><td><input type="hidden" name="on0" value="Select Class Here"></input>Select Class Here</td></tr><tr><td><select name="os0">
+										<option value="Single Class">Single Class $10.87 USD</option>
+										<option value="5 Class Package">5 Class Package $41.95 USD</option>
+										<option value="10 Class Package">10 Class Package $62.68 USD</option>
+										<option value="30 Day Access Pass">30 Day Access Pass $78.22 USD</option>
+										<option value="2 week Challenge">2 week Challenge $52.32 USD</option>
+										<option value="4 week Challenge">4 week Challenge $83.40 USD</option>
+										<option value="6 week Challenge">6 week Challenge $104.12 USD</option>
+									</select> </td></tr>
+								</table>
+								<input type="hidden" name="currency_code" value="USD"></input>
+								<input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_paynowCC_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!"></input>
+								<img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1"></img>
+							</form>
+
 							</div>
 						</div>
 					</div>
